e63f34043e34769ccd69e703664775ff5c63136b
defreezz <yatsevich-artsiom@yandex.by>
2021-12-20 15:50:13 +0300

    getLocalStorageHandler changed

--- src/App.tsx
+++ src/App.tsx
@@ -33,9 +33,9 @@ function App () {
         setMaxValue(max && JSON.parse(max))
         setStartValue(min && JSON.parse(min))
         setCounter(min && JSON.parse(min))
-        setCounter(0)
-        setStartValue(0)
-        setMaxValue(0)
+        max ===null && setCounter(0)
+        max===null &&setStartValue(0)
+        max===null &&setMaxValue(0)
     }
 
 
