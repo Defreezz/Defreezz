d1fd82b788111d62df451c3d40a30786c7ef2ba9
defreezz <yatsevich-artsiom@yandex.by>
2021-12-25 20:33:49 +0300

    added 2 versions of the counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,68 @@
-import React, {ChangeEvent, useEffect, useState} from 'react';
+import React, { useEffect, useState} from 'react';
 import './App.css';
-import {Display} from "./Tablo/Display";
-import {Button} from "./Buttons/Button/Button";
+import {CounterV2} from "./Counter_v2";
+import {Switch} from "@material-ui/core";
+import {CounterV1} from "./Counter_v1";
 
 
+function App() {
+//логика отображения версиии счетчика
+    const [changeVersion, setChangeVersion] = useState<boolean>(false)
 
-function App () {
-    //BLL
-
-    const [counter, setCounter] = useState<number>(0)
-    const inc = () => {if (counter < maxValue) {setCounter(counter + 1) }}
-    const reset = () => {setCounter(startValue)}
-
-
-    const  [startValue,setStartValue] = useState<number>(0)
-
-
-    const [maxValue, setMaxValue] = useState<number>(0)
-    const maxValueHandler = (e:ChangeEvent<HTMLInputElement>) => {setMaxValue(Number(e.currentTarget.value))}
-
-
-    //  локалстораж
-    const setLocalStorage = () => {
-        localStorage.setItem("MaxValue",JSON.stringify(maxValue))
-        localStorage.setItem("StartValue",JSON.stringify(startValue))
-    }
-    useEffect(() => {getLocalStorageHandler()},[])
-
+//localstorage
+    const setLocalStorage = (version:boolean) => {
+        localStorage.setItem("VersionCounter", JSON.stringify(version))    }
 
     const getLocalStorageHandler = () => {
-        let max = localStorage.getItem("MaxValue")
-        let min = localStorage.getItem("StartValue")
-        setMaxValue(max && JSON.parse(max))
-        setStartValue(min && JSON.parse(min))
-        setCounter(min && JSON.parse(min))
-        max ===null && setCounter(0)
-        max===null &&setStartValue(0)
-        max===null &&setMaxValue(0)
+        let version = localStorage.getItem("VersionCounter")
+        setChangeVersion(version && JSON.parse(version))
+
     }
 
-
-    // стейт отображения настроек счетчика
-    const [mode, setMode] = useState<boolean>(true)
-    const saveSettings = () => {
-        setMode(!mode)
-        setLocalStorage()
-        setCounter(startValue)
-    }
-//логика ошибки
+    useEffect(() => {
+        getLocalStorageHandler()
+        //setLocalStorage()
+    }, [])
+    useEffect(() => {
+        setLocalStorage(changeVersion)
+    }, [changeVersion])
 
 
-    //UI
+
+//UI
+
     return (
-        mode
+        changeVersion
             ? <div className="App">
-                <div className={"wrapper"}>
-                    <Display value={counter} maxValue={maxValue} startValue={startValue}/>
-                    <span className={"buttContainer"}>
-                    <Button name={"INC"} callback={inc} disabled={counter >= maxValue}/>
-                    <Button name={"RESET"} callback={reset} disabled={counter === startValue}/>
-                    <Button name={"SET"} callback={() => {setMode(!mode)}}/>
+                <div className={"switch"}>
+                <span>
+                    v1
+                    <Switch
+                        checked={changeVersion}
+                        onChange={() => { setChangeVersion(!changeVersion);setLocalStorage(changeVersion)}}
+                    />
+                    v2
                 </span>
                 </div>
+                <CounterV2/>
             </div>
             : <div className="App">
-                <div className={"wrapper"}>
-                    <div className={"display"}>
-
-                        <span className={maxValue<=startValue?"spanError":""}>Max Value </span>
-                        <input
-                            value={maxValue}
-                            className={maxValue<=startValue?"inputSetError":"inputSet"}
-                            type={"number"}
-                            onChange={maxValueHandler}
-                        />
-                        <br/>
-                        <span className={maxValue<=startValue?"spanError":""}>Start Value</span>
-                        <input
-                            value={startValue}
-                            className={maxValue<=startValue || startValue < 0?"inputSetError":"inputSet"}
-                            type={"number"}
-                            onChange={(e)=>{setStartValue(Number(e.currentTarget.value)); setCounter(Number(e.currentTarget.value))}}
-                        />
-                    </div>
-
-                    <span className={"buttContainerSet"}>
-                    <Button
-                        disabled={startValue >= maxValue || startValue < 0}
-                        name={"SAVE"}
-                        callback={saveSettings}
-
+                <div className={"switch"}>
+                   <span>
+                    v1
+                    <Switch
+                        checked={changeVersion}
+                        onChange={() => {setChangeVersion(!changeVersion);setLocalStorage(changeVersion)}}
                     />
+                    v2
                 </span>
+
+
                 </div>
+                <CounterV1/>
             </div>
     )
+
 
 }
 
---
+++ src/Counter_v1.tsx
@@ -0,0 +1,106 @@
+import React, {ChangeEvent, useEffect, useState} from "react";
+import {Display} from "./Tablo/Display";
+import {Button} from "./Buttons/Button/Button";
+
+export function CounterV1() {
+    //BLL
+
+    const [counter, setCounter] = useState<number>(0)
+    const inc = () => {
+        if (counter < maxValue) {
+            setCounter(counter + 1)
+        }
+    }
+    const reset = () => {
+        setCounter(startValue)
+    }
+
+
+    const [startValue, setStartValue] = useState<number>(0)
+
+
+    const [maxValue, setMaxValue] = useState<number>(0)
+    const maxValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setMaxValue(Number(e.currentTarget.value))
+    }
+
+
+    //  localstorage
+    const setLocalStorage = () => {
+        localStorage.setItem("MaxValue", JSON.stringify(maxValue))
+        localStorage.setItem("StartValue", JSON.stringify(startValue))
+    }
+    useEffect(() => {
+        getLocalStorageHandler()
+    }, [])
+
+
+    const getLocalStorageHandler = () => {
+        let max = localStorage.getItem("MaxValue")
+        let min = localStorage.getItem("StartValue")
+        setMaxValue(max && JSON.parse(max))
+        setStartValue(min && JSON.parse(min))
+        setCounter(min && JSON.parse(min))
+        max === null && setCounter(0)
+        max === null && setStartValue(0)
+        max === null && setMaxValue(0)
+    }
+
+    const saveSettings = () => {
+        setLocalStorage()
+        setCounter(startValue)
+    }
+//логика ошибки
+
+
+    //UI
+    return (
+        <div className={"v1"}>
+
+            <div className={"wrapper"}>
+                <div className={"display"}>
+
+                    <span className={maxValue <= startValue ? "spanError" : ""}>Max Value </span>
+                    <input
+                        value={maxValue}
+                        className={maxValue <= startValue ? "inputSetError" : "inputSet"}
+                        type={"number"}
+                        onChange={maxValueHandler}
+                    />
+                    <br/>
+                    <span className={maxValue <= startValue ? "spanError" : ""}>Start Value</span>
+                    <input
+                        value={startValue}
+                        className={maxValue <= startValue || startValue < 0 ? "inputSetError" : "inputSet"}
+                        type={"number"}
+                        onChange={(e) => {
+                            setStartValue(Number(e.currentTarget.value));
+                            setCounter(Number(e.currentTarget.value))
+                        }}
+                    />
+                </div>
+
+                <span className={"buttContainerSet"}>
+                    <Button
+                        disabled={startValue >= maxValue || startValue < 0}
+                        name={"SAVE"}
+                        callback={saveSettings}
+
+                    />
+                </span>
+            </div>
+            <div>
+                <div className={"wrapper"}>
+                    <Display value={counter} maxValue={maxValue} startValue={startValue}/>
+                    <span className={"buttContainer"}>
+                    <Button name={"INC"} callback={inc} disabled={counter >= maxValue}/>
+                    <Button name={"RESET"} callback={reset} disabled={counter === startValue}/>
+                </span>
+                </div>
+            </div>
+
+        </div>
+
+    )
+
+}
